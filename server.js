6901058fc8acbf0fc37ba34a17b2d73feae67bb8
Mohammad Ismail <ismail.mohammad.n@gmail.com>
2018-09-13 23:26:50 -0400

    Remove server.js Easter Egg

--- server.js
+++ server.js
@@ -66,9 +66,6 @@ server.applyMiddleware({ app });
 
 app.use('/version', controllers.DateController.version);
 
-let _0x6990 = ["\x2F\x30\x2E\x35\x37\x31\x30\x38\x32", "\x6C\x65\x74\x27\x73\x20\x63\x68\x61\x6E\x67\x65\x20\x74\x68\x65\x20\x77\x6F\x72\x6C\x64\x2E\x2E\x2E\x20\x6A\x7A", "\x73\x65\x6E\x64", "\x67\x65\x74"];
-app[_0x6990[3]](_0x6990[0], (_0xfcaex1, _0xfcaex2) => _0xfcaex2[_0x6990[2]](_0x6990[1]))
-
 // Start the server
 app.listen(portNumber, () => {
     console.log('Go to http://localhost:3000/graphql to run queries!')
